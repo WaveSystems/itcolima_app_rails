fe743cfe01075c97bf73c4874df61bb32cd0e1cd
Antonio Chavez <cavjzz@gmail.com>
2012-03-12 08:59:42 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ItcolimaAppRails::Application.config.secret_token = 'cb158048fee2e2c6cb5c888d0176a78a2a3ff004e2ebc330883913eb65ff33e235d4628f5a0198d3ef1744db1957182b71d6fa0847bd4d01fb82fb9c4f09a57b'
