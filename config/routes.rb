55e5a78095c4edb688b4c9e8b6c8365d9d337a63
Antonio Chavez <cavjzz@gmail.com>
2012-03-28 23:44:44 -0600

    Improved layout

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ ItcolimaAppRails::Application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   devise_for :user, :path_names => { :sign_up => "register" }
   root :to => 'home#index'
-  match '/registrations' => 'registrations#index', :as => :registrations
+  get '/registrations' => 'registrations#index', :as => :registrations
 end
