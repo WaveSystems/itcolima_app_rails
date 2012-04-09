5ca1a67ae3e4904f0baeda3b0e9515d018556c61
Antonio Chavez <cavjzz@gmail.com>
2012-04-09 15:54:52 -0500

    Added new routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ ItcolimaAppRails::Application.routes.draw do
   get '/registrations' => 'registrations#index', :as => :registrations
   get '/new_project' => 'registrations#new', :as => :new_project
   post '/create_project' => 'registrations#create', :as => :create_project
+  get '/new_project/:proyecto_id/alumnos' => 'registrations#search_for_alumnos', :as => :search_for_alumnos
+  post '/new_project/:proyecto_id/alumnos/:alumno_id' => 'registrations#edit_alumno', :as => :edit_alumno
+
   get '/about' => 'home#about', :as => :about
   get '/contact' => 'home#contact', :as => :contact
 end
