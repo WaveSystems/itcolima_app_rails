17550014027f75bbfe6d1c6df74f5ed6ca31dfb0
Antonio Chavez <cavjzz@gmail.com>
2012-04-09 15:54:14 -0500

    Added accessible attributes

--- app/models/proyecto.rb
+++ app/models/proyecto.rb
@@ -1,7 +1,7 @@
 class Proyecto < ActiveRecord::Base
   belongs_to :asesor
   has_many :alumnos
-  validates_presence_of :categoria, :nombre, :area_participacion, :objetivo, :innovacion, :resultados_esperados, :numero_de_autores, :asesor_id, :numero_asesores
+  validates_presence_of :categoria, :nombre, :area_participacion, :objetivo, :innovacion, :resultados_esperados, :numero_de_autores, :numero_asesores
   attr_accessible :categoria, :nombre, :area_participacion, :objetivo, :innovacion, :resultados_esperados, :numero_de_autores, :asesor_id, :numero_asesores
   CATEGORIAS = %w[Producto Servicio Proceso]
   AREAS_DE_PARTICIPACION = %w[Agua Salud Medio\ ambiente Alimentos Agricultura Transporte Energia Comunicaciones]
