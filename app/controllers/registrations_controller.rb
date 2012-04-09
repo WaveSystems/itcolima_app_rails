0c6b896eac44b562fa0e2ab4fc3597f8ca1f3f28
Antonio Chavez <cavjzz@gmail.com>
2012-04-09 15:54:29 -0500

    Added new forms for alumnos updating

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -11,10 +11,24 @@ class RegistrationsController < ApplicationController
     @proyecto = Proyecto.new(params[:proyecto])
     if @proyecto.save
       flash[:notice]="El proyecto ha sido creado exitosamente"
-      redirect_to :registrations
+      redirect_to search_for_alumnos_path(@proyecto.id)
     else
       flash[:alert]="Ups! Algo salio mal, intente nuevamente..."
       redirect_to :new_project
     end
+  end
+
+  def search_for_alumnos
+    @proyecto = Proyecto.find(params[:proyecto_id])
+    if @proyecto.numero_de_autores == @proyecto.alumnos.count
+      flash[:notice]="Porfavor ingrese los datos de los asesores"
+      redirect_to :registrations
+    end
+  end
+
+  def edit_alumno
+  end
+
+  def update_alumno
   end
 end
