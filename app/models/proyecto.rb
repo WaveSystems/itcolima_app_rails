b81f8df3d6eae848a7eeba88a51e938d654fbb7b
Antonio Chavez <cavjzz@gmail.com>
2012-04-09 15:25:38 -0500

    Added mass assignment security

--- app/models/proyecto.rb
+++ app/models/proyecto.rb
@@ -1,6 +1,8 @@
 class Proyecto < ActiveRecord::Base
   belongs_to :asesor
   has_many :alumnos
+  validates_presence_of :categoria, :nombre, :area_participacion, :objetivo, :innovacion, :resultados_esperados, :numero_de_autores, :asesor_id, :numero_asesores
+  attr_accessible :categoria, :nombre, :area_participacion, :objetivo, :innovacion, :resultados_esperados, :numero_de_autores, :asesor_id, :numero_asesores
   CATEGORIAS = %w[Producto Servicio Proceso]
   AREAS_DE_PARTICIPACION = %w[Agua Salud Medio\ ambiente Alimentos Agricultura Transporte Energia Comunicaciones]
 end
