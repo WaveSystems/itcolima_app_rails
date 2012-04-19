27a098cbc48600bc5d0c0d64d906d96540b770ea
Antonio Chavez <cavjzz@gmail.com>
2012-04-19 12:54:15 -0500

    Added name for asesor table

--- app/models/asesor.rb
+++ app/models/asesor.rb
@@ -2,7 +2,7 @@ class Asesor < ActiveRecord::Base
   belongs_to :proyecto
   NIVEL_ACADEMICO = %w[Maestria Licenciatura Doctorado]
   TIPO_ASESOR = %w[Interno Externo]
-  attr_accessible :titulo, :apellido_paterno, :apellido_materno, :nombre, :departamento, :no_ife, :email, :curp, :nivel_academico
+  attr_accessible :titulo, :apellido_paterno, :apellido_materno, :nombre, :departamento, :no_ife, :email, :curp, :nivel_academico, :tipo_asesor, :proyecto_id
 
   def self.asesores
     asesores = Asesor.all
