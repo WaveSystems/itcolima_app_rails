d23a54943d00ddbf9fdf49c129c8ded79897c579
Antonio Chavez <cavjzz@gmail.com>
2012-05-17 15:48:40 -0500

    Fixed requerimientos model

--- app/models/proyecto.rb
+++ app/models/proyecto.rb
@@ -2,10 +2,10 @@ class Proyecto < ActiveRecord::Base
   has_many :asesors
   has_many :alumno_proyectos
   has_many :alumnos, :through => :alumno_proyectos
-  validates_presence_of :categoria, :nombre, :area_participacion, :objetivo, :innovacion, :resultados_esperados, :numero_de_autores, :numero_asesores
+  validates_presence_of :categoria, :nombre, :area_participacion, :objetivo, :innovacion, :resultados_esperados, :numero_de_autores, :numero_asesores, :requerimentos
   attr_accessible :categoria, :nombre, :area_participacion, :objetivo, :innovacion, :resultados_esperados, :numero_de_autores, :asesor_id, :numero_asesores, :finished, :requerimentos
   CATEGORIAS = %w[Producto Servicio Proceso]
-  AREAS_DE_PARTICIPACION = %w[Agua Salud Medio\ ambiente Alimentos Agricultura Transporte Energia Comunicaciones]
+  AREAS_DE_PARTICIPACION = %w[Expo\ feria Agua Salud Medio\ ambiente Alimentos Agricultura Transporte Energia Comunicaciones]
 
   def self.proyectos
     proyectos = Proyecto.all
